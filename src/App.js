fbca1900de4fdd2d2546848fe66cb3d24321eafc
이소은 <soeun2537@naver.com>
2023-08-16 01:33:25 +0900

    feat: 홈페이지 버튼 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,52 @@
+import { useState } from "react";
+import "./App.css";
+import Button from "./Button";
+
 function App() {
-  return <div>hello world</div>;
+  return (
+    <div className="main">
+      <Button
+        url="https://portal.uos.ac.kr/user/login.face"
+        imgUrl="img/uos.png"
+        title="서울시립대학교 포털"
+      />
+      <Button
+        url="https://www.youtube.com/"
+        imgUrl="img/youtube.png"
+        title="유튜브"
+      />
+      <Button
+        url="https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%EB%A7%9E%EC%B6%A4%EB%B2%95+%EA%B2%80%EC%82%AC%EA%B8%B0&oquery=%EB%A7%9E%EC%B6%A4%EB%B2%95%EA%B2%80%EC%82%AC%EA%B8%B0&tqi=itS%2Ffsp0J1sssdIzUYGsssssszd-138875"
+        imgUrl="img/naver.jpg"
+        title="맞춤법 검사기"
+      />
+      <Button
+        url="https://www.google.com/search?q=%EA%B5%AC%EA%B8%80%EB%B2%88%EC%97%AD%EA%B8%B0&oq=%EA%B5%AC%EA%B8%80%EB%B2%88%EC%97%AD%EA%B8%B0&aqs=chrome..69i57j0i131i433i512j46i131i199i433i465i512j0i131i433i512l2j46i199i465i512j46i131i199i433i465i512j46i199i465i512j0i512j0i131i433i512.1430j0j15&sourceid=chrome&ie=UTF-8"
+        imgUrl="img/google.png"
+        title="구글번역기"
+      />
+      <Button
+        url="https://chat.openai.com/"
+        imgUrl="img/chatGPT.png"
+        title="chatGPT"
+      />
+      <Button
+        url="https://nomadcoders.co/"
+        imgUrl="img/nomadcoders.png"
+        title="노마드코더"
+      />
+      <Button
+        url="https://opentutorials.org/course/1"
+        imgUrl="img/생활코딩.png"
+        title="생활코딩"
+      />
+      <Button
+        url="https://maplestory.nexon.com/Home/Main"
+        imgUrl="img/maple.jpg"
+        title="메이플스토리"
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Button.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+function Button({ url, imgUrl, title }) {
+  const [mouseState, setMouseState] = useState(false);
+  const onMouseOver = () => {
+    setMouseState(true);
+  };
+  const onMouseOut = () => {
+    setMouseState(false);
+  };
+  return (
+    <a
+      onMouseOver={onMouseOver}
+      onMouseOut={onMouseOut}
+      className="tile"
+      style={{ backgroundColor: mouseState ? "#d2d2d2" : "white" }}
+      href={url}
+    >
+      <img className="tileImg" src={imgUrl}></img>
+      <div className="tileTitle">{title}</div>
+    </a>
+  );
+}
+
+export default Button;
