d8a9e96cd2fd7ee198baea20ac62561297893d62
이소은 <soeun2537@naver.com>
2023-08-16 17:23:19 +0900

    chore: 이미지 추가 및 수정

--- src/components/Background.js
+++ src/components/Background.js
@@ -12,6 +12,16 @@ const images = [
   "11.jpg",
   "12.jpg",
   "13.jpg",
+  "14.jpg",
+  "15.jpg",
+  "16.jpg",
+  "17.jpg",
+  "18.jpg",
+  "19.jpg",
+  "20.jpg",
+  "21.jpg",
+  "22.jpg",
+  "23.jpg",
 ];
 
 function Background() {
